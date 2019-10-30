eb38dad114b61fcbf0c7e1eac96786dd4a3a5c84
marty1885 <marty188586@gmail.com>
2019-10-31 01:05:37 +0800

    add options to generator

--- genbinding.py
+++ genbinding.py
@@ -3,27 +3,56 @@ import subprocess, sys
 from pathlib import Path
 import argparse
 
-#parser = argparse.ArgumentParser(description='Etaler Python binding generator')
-#parser.add_argument('--home', dest='home', '')
+try:
+    import cppyy
+except:
+    print('Failed to import cppyy. Please make sure cppyy is installed correctly')
+    exit(1)
 
-etaler_home = "/usr/local/include"
+parser = argparse.ArgumentParser(description='Etaler Python binding generator')
+parser.add_argument('--home', dest='home', default=None, help='Where Etaler\'s header folder is located')
+parser.add_argument('--cxx', dest='cxx', default='c++',  help='the c++ compiler you use')
+
+args = parser.parse_args()
+
+user_home = args.home
+cxx = args.cxx
 rfldct = 'etaler'
 
-# First we generate the redlection data
-etaler_headers = [os.path.relpath(x, etaler_home) for x in Path(os.path.join(etaler_home, 'Etaler')).rglob('*.hpp')]
+etaler_homes = ['/usr/local/include', '/usr/include'] if user_home is None else [user_home]
+etaler_headers = []
+etaler_home = ''
+# Attempt to find a valid home
+for home in etaler_homes:
+    etaler_headers = [os.path.relpath(x, home) for x in
+            Path(os.path.join(home, 'Etaler')).rglob('*.hpp')]
+    if len(etaler_headers) != 0:
+        etaler_home = home
+        break
+if etaler_home == '': # not found
+    raise IOError('Cannot find Etaler headers in ' + str(etaler_homes))
 
 # Filter unwanted headers
 etaler_headers = [x for x in etaler_headers if
         'OpenCL' not in x and         # FIXME: Backends causes loading problem
-        'Interop' not in x and        # Disable interop
-        'Util' not in x and           # uses std::chrono, but not supported by cppyy
-        'CPU' not in x                # Same issue, causes loading problem
+        'CPU' not in x and            # Same issue, causes loading problem
+        'Interop' not in x and        # Disable interop. TODO: add option to enable
+        'ProgressDisplay' not in x    # uses std::chrono, but not supported by cppyy
     ]
 
+
+print('---------generator information----------')
+print('Etaler home   : %s'%etaler_home)
+print('Interop       : Not supported now')
+print('Backends      : Not supported now')
+print('C++ compiler  : %s'%cxx)
+print('Generating via cppyy...')
+
+# First we generate the redlection data
 cmd = ' '.join(
     ['genreflex',                     # utility installed by pip when installing cppyy
      '--verbose',                     # Show information (somehow genreflex fail without this)
-     '-s', 'selection.xml',           # election file
+     '-s', 'selection.xml',           # selection file
      '-o', '%s_rflx.cpp'%rfldct]+     # intermediate output file
      etaler_headers)                  # headers themselves
 ret = os.system(cmd)
@@ -34,16 +63,18 @@ else:
     print("genreflex done")
 
 # Next we build the Python module itself
+print('Compiling binding...')
 clingflags = subprocess.check_output(
     ['cling-config',               # utility installed by pip when installing cppyy
     '--cppflags'])
 
 try:
     subprocess.check_output(
-        ['c++']+                       # C++ compiler
+        [cxx]+                         # C++ compiler
          clingflags.split()+[          # extra flags provided by cling
          '-fPIC',                      # require position independent code
          '-shared',                    # generate shared library
+         '-std=c++1z',                 # cppyy should have set this but to be safe.
          '-o', '%s_rflx.so'%rfldct,    # output file
          '-I'+etaler_home,             # include search path for Etaler headers
          '%s_rflx.cpp'%rfldct]+        # intermediate file to compile
