827539430ae49a8eebd8121745f6883cdda0db45
marty1885 <marty188586@gmail.com>
2019-11-26 16:25:57 +0800

    add importing std namespace

--- etaler/__init__.py
+++ etaler/__init__.py
@@ -6,6 +6,7 @@ cppyy.load_reflection_info(os.path.join(src_dir, "etaler_rflx.so"))
 
 
 from cppyy.gbl import et
+from cppyy.gbl import std
 
 # Override the default __repr__ with Etaler's function
 et.Shape.__repr__ = lambda self: cppyy.gbl.cling.printValue(self)
